3a354892c2ac7eea56bc590b6c22fee536357153
tiantu23 <3339363562@qq.com>
2026-04-14 00:21:18 +0800

    Delete enPro directory

--- enPro/envPro/js/dingbu.white.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-        if ($(this).scrollTop() > 50) {
-            $('.clean-main-menu').addClass('scrolled');
-        } else {
-            $('.clean-main-menu').removeClass('scrolled');
-        }
-    });
